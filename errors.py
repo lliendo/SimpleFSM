8b5746585d665102b7151d29cbe16302ab24fe5d
Lucas Liendo <mindmaster@gmail.com>
2014-06-14 18:49:55 +1200

    - All exceptions now inherit from FSMError exception.

--- errors.py
+++ errors.py
@@ -1,51 +1,51 @@
-class FSMError(Exception) :
+class FSMError(Exception):
     pass
 
 
-class FSMStartStatesError(FSMError) :
-    def __str__(self) :
+class FSMStartStatesError(FSMError):
+    def __str__(self):
         return "Error - Only one start state is allowed for this FSM."
 
 
-class FSMNoStartStateError(FSMError) :
-    def __str__(self) :
+class FSMNoStartStateError(FSMError):
+    def __str__(self):
         return "Error - No start state defined for this FSM."
 
 
-class FSMFinalStateError(FSMError) :
-    def __str__(self) :
+class FSMFinalStateError(FSMError):
+    def __str__(self):
         return "Error - No final state/s defined for this FSM."
 
 
-class FSMUnimplementedInput(FSMError) :
-    def __str__(self) :
+class FSMUnimplementedInput(FSMError):
+    def __str__(self):
         return "Error - The read_symbol() method must be implemented."
 
 
-class FSMRejectedInput(Exception) :
-    def __init__(self, symbol) :
+class FSMRejectedInput(FSMError):
+    def __init__(self, symbol):
         self._symbol = symbol
 
-    def __str__(self) :
-        return "'%s' is not a recognized symbol by this FSM." % self._symbol
+    def __str__(self):
+        return "'{0}' is not a recognized symbol by this FSM.".format(self._symbol)
 
 
-class FSMEndOfInput(Exception) :
-    def __str__(self) :
+class FSMEndOfInput(FSMError):
+    def __str__(self):
         return "No more symbols available. FSM reached input end."
 
 
-class FSMDuplicatedState(Exception) :
-    def __init__(self, state) :
+class FSMDuplicatedState(FSMError):
+    def __init__(self, state):
         self._state = state
 
-    def __str__(self) :
-        return "Error - The state : '%s' is already defined." % self._state.id
+    def __str__(self):
+        return "Error - The state : '{0}' is already defined.".format(self._state.id)
 
 
-class FSMDuplicatedTransition(Exception) :
-    def __init__(self, transition) :
+class FSMDuplicatedTransition(FSMError):
+    def __init__(self, transition):
         self._transition = transition
 
-    def __str__(self) :
-        return "Error - There's already a transition from '%s' to '%s' with the same function." % self._transition.from_state.id, self._transition.to_state.id
+    def __str__(self):
+        return "Error - There's already a transition from '{0}' to '{1}' with the same function.".format(self._transition.from_state.id, self._transition.to_state.id)
