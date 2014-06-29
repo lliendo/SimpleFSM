369be2de39f86d8ebd9eb7c72859fdd452421573
Lucas Liendo <mindmaster@gmail.com>
2014-06-29 16:32:09 +1200

    - Changed initial version to 0.0.1.  - Added zip_safe option to setup.  - Changed packages option to point just to simplefsm's module directory.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from setuptools import setup, find_packages
-
-
-TEST_DIRS = ["*.tests", "*.tests.*", "tests.*", "tests"]
+from setuptools import setup
 
 setup(
-    name = "SimpleFSM",
-    description = "A simple package that allows you model arbitrary automatons.", 
-    version = "0.1",
-    packages = find_packages(exclude=TEST_DIRS),
-    author = "Lucas Liendo",
-    author_email = "mindmaster@gmail.com",
-    keywords = "automaton finite state machine",
-    install_requires = ["nose >= 1.3.2"], 
-    test_suite = "nose.collector",
+    name="SimpleFSM",
+    description="A simple package that allows you model arbitrary automatons.", 
+    version="0.0.1",
+    packages = ["simplefsm"],
+    author="Lucas Liendo",
+    author_email="mindmaster@gmail.com",
+    keywords="automaton finite state machine",
+    install_requires=["nose>=1.3.2"], 
+    zip_safe=False,
+    test_suite="nose.collector",
 )
