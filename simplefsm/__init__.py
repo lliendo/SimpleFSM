f77b21711bbd815f4cedb8f2642d328f9875d7ec
Lucas Liendo <mindmaster@gmail.com>
2014-07-25 19:27:40 +1200

    - Added UTF8 encoding directive. - Added LGPL v3 statement.

--- simplefsm/__init__.py
+++ simplefsm/__init__.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of SimpleFSM.
+
+SimpleFSM is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SimpleFSM is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with SimpleFSM. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2014 Lucas Liendo.
+"""
+
 from abc import ABCMeta, abstractmethod
 from exceptions import *
 
