9f71495d52c5d091d0e5285ce10317eaeddb51ad
Lucas Liendo <mindmaster@gmail.com>
2015-02-03 14:58:56 -0300

    - Added rejected_input property to FSMRejectedinput exception.  - Updated __str__() method.

--- simplefsm/exceptions.py
+++ simplefsm/exceptions.py
@@ -25,36 +25,43 @@ class FSMError(Exception):
 
 class FSMStartStatesError(FSMError):
     def __str__(self):
-        return "Error - Only one start state is allowed for this FSM."
+        return 'Error - Only one start state is allowed for this FSM.'
 
 
 class FSMNoStartStateError(FSMError):
     def __str__(self):
-        return "Error - No start state defined for this FSM."
+        return 'Error - No start state defined for this FSM.'
 
 
 class FSMFinalStateError(FSMError):
     def __str__(self):
-        return "Error - No final state/s defined for this FSM."
+        return 'Error - No final state/s defined for this FSM.'
 
 
 class FSMNotImplementedInput(FSMError):
     def __str__(self):
-        return "Error - The read_symbol() method must be implemented."
+        return 'Error - The read_symbol() method must be implemented.'
 
 
 class FSMRejectedInput(FSMError):
-    def __init__(self, symbol, type="symbol"):
-        self._symbol = symbol
+    def __init__(self, rejected_input, type='symbol'):
+        self._rejected_input = rejected_input
         self._type = type
 
+    @property
+    def rejected_input(self):
+        return self._rejected_input
+
     def __str__(self):
-        return "'{0}' is not a recognized {1} by this FSM.".format(self._symbol, self._type)
+        rejected_input_formatted = ' '.join('{0}'.format(s) for s in self._rejected_input)
+        return 'Error - \'{0}\' is not a recognized {1} by this FSM.'.format(
+            rejected_input_formatted, self._type
+        )
 
 
 class FSMEndOfInput(FSMError):
     def __str__(self):
-        return "No more symbols available. FSM reached input end."
+        return 'No more symbols available. FSM reached input end.'
 
 
 class FSMDuplicatedState(FSMError):
@@ -62,7 +69,7 @@ class FSMDuplicatedState(FSMError):
         self._state = state
 
     def __str__(self):
-        return "Error - The state : '{0}' is already defined.".format(self._state.id)
+        return 'Error - The state : \'{0}\' is already defined.'.format(self._state.id)
 
 
 class FSMDuplicatedTransition(FSMError):
@@ -70,6 +77,6 @@ class FSMDuplicatedTransition(FSMError):
         self._transition = transition
 
     def __str__(self):
-        return "Error - There's already a transition from '{0}' to '{1}' with the same function.".format(
+        return 'Error - There\'s already a transition from \'{0}\' to \'{1}\' with the same function.'.format(
             self._transition.from_state.id, self._transition.to_state.id
         )
